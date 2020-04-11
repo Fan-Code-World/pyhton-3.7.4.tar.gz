1077dda418bcae86d46bb6b47c4e44e1cdf8fc34
woider <woider@gmail.com>
2020-04-11 19:15:46 +0800

    install

---
+++ 20200411install.sh
@@ -0,0 +1,9 @@
+yum -y install zlib-devel bzip2-devel openssl-devel ncurses-devel sqlite-devel readline-devel tk-devel gdbm-devel db4-devel libpcap-devel xz-devel
+yum install libffi-devel -y
+yum install libxml*
+./configure --prefix=/usr/local/python3 --with-ssl
+make
+make install
+ln -s /usr/local/python3/bin/python3 /usr/local/bin/python3
+ln -s /usr/local/python3/bin/pip3 /usr/local/bin/pip3 
+
